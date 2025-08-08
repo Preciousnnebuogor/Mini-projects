eb120594d73529c57919a88f572a1984e0821a4d
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-08-07 18:35:18 -0700

    collected

--- src/components/popfi/pop.jsx
+++ src/components/popfi/pop.jsx
@@ -2,27 +2,40 @@ import { useState } from "react";
 import "./pop.css";
 export default function PopFi() {
   const [ball, setBall] = useState(true);
+  const [canPop, setCanPop] = useState(true)
+  const [itemLeft, setItemLeft] = useState(0)
 
   function handleBaloon() {
+  if(!canPop) return
+
         //setBall(true)
       setBall((prev) => !prev);
+      setCanPop(false)
+
+      setTimeout(() => {
+        setCanPop(true)
+      }, 5 * 60 *1000);
+
+      let second = 5* 60
+      setItemLeft(second)
+
+      const countDown = setTimeout(() => {
+        second -= 1
+        setItemLeft(second)
+        if(second <= 0) {
+            clearInterval(countDown)
+        }
+      }, 1000);
   }
 
-//   setTimeout(() => {
-//     //setBall(true)
-//     setBall((prev) => !prev);
-//   }, 2000);
-
-  function handleTime(){
-    if(ball) {
-        
-    }
-
-  }
 
   return (
     <div className="container">
-      <div>Time</div>
+      <div>
+        {canPop
+          ? "You can pop the balloon!"
+          : `Wait ${timeLeft} seconds to pop again.`}
+      </div>
       <div className="content">
         <img
           src="./assets/baloon.jpeg"
