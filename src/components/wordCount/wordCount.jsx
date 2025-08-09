079f9dedf273b2d9f55bbcba34900479d8115cd3
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-08-09 15:06:42 -0700

    collected

--- src/components/wordCount/wordCount.jsx
+++ src/components/wordCount/wordCount.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
-import "./wordCount.css"
-export default function WordCount(){
-    const[text, setText] = useState('')
-    const [count, setCount] = useState('')
-    return (
-      <div className="container">
-        <div className="content">
-          <div className="top">
-            <p>Real- Time Character Counter</p>
-            <textarea />
-          </div>
-          <div className="bottom">
-            <p>Total Character: 50 </p>
-            <p>Remaining: 0</p>
-          </div>
+import "./wordCount.css";
+export default function WordCount() {
+  const [text, setText] = useState("");
+  const [count, setCount] = useState("");
+
+  function countCharacter() {
+    setCount(text.length);
+  }
+
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="top">
+          <p>Real- Time Character Counter</p>
+          <textarea
+            maxLength={50}
+            onChange={(e) => {
+              setText(e.target.value);
+              {
+                countCharacter();
+              }
+            }}
+            value={text}
+          />
+        </div>
+        <div className="bottom">
+          <p>Total Character:{count} </p>
+          <p>Remaining: {50 - count}</p>
         </div>
       </div>
-    );
+    </div>
+  );
 }
